62a688f673fa619e2a9ba574862442e77ad40281
erizta <eriztaalifad@gmail.com>
2023-05-19 06:14:01 +0700

    Add Slider Select

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Grid } from "@mui/material";
 // components
 import Navbar from "./components/Navbar";
@@ -7,13 +7,21 @@ import SliderSelect from "./components/SliderSelect";
 import TenureSelect from "./components/TenureSelect";
 
 function App() {
+  const [data, setData] = useState({
+    homeValue: 3000,
+    downPayment: 3000 * 0.2,
+    loanAmount: 3000 * 0.8,
+    loanTerm: 5,
+    interestRate: 5,
+  });
+
   return (
     <div>
       <Navbar />
       <Container maxWidth="xl" sx={{ marginTop: 4 }}>
         <Grid container spacing={5} alignItems="center">
           <Grid item xs={12} md={6}>
-            <SliderSelect />
+            <SliderSelect data={data} setData={setData} />
             <TenureSelect />
           </Grid>
           <Grid item xs={12} md={6}>
