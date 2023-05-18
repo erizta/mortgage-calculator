c8446830e5a48bf9c050af26988f0a4b9bc6eb6a
erizta <eriztaalifad@gmail.com>
2023-05-19 06:09:33 +0700

    Add Slider Component

--- src/components/Common/SliderComponent.js
+++ src/components/Common/SliderComponent.js
@@ -1,5 +1,45 @@
-const SliderComponent = () => {
-  return <></>;
+import { Slider, Stack, Typography } from "@mui/material";
+
+const SliderComponent = ({
+  defaultValue,
+  min,
+  max,
+  label,
+  unit,
+  steps,
+  amount,
+  onChange,
+  value,
+}) => {
+  return (
+    <Stack my={1.4}>
+      <Stack gap={1}>
+        <Typography variant="subtitle2">{label}</Typography>
+        <Typography variant="h5">
+          {unit} {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        onChange={onChange}
+        value={value}
+        marks
+        step={steps}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          {unit} {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit} {max}
+        </Typography>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default SliderComponent;
