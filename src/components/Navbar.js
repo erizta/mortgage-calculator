354072318f53ba92e3e43c4f4ef62edcba929fc7
erizta <eriztaalifad@gmail.com>
2023-05-19 06:00:04 +0700

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,43 @@
 import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { keyframes } from "@emotion/react";
+
+// Define the gradient animation
+const gradientAnimation = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
+// Create a styled component with the gradient background
+const StyledAppBar = styled(AppBar)`
+  background: linear-gradient(45deg, #b4e1f0 30%, #b9e3c6 90%);
+  animation: ${gradientAnimation} 10s infinite;
+`;
+
+const StyledTypography = styled(Typography)`
+  color: #333;
+  font-family: "Poppins", sans-serif;
+`;
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
+    <StyledAppBar position="static">
       <Container maxWidth="xl">
         <Toolbar>
-          <Typography variant="h5">A & H Bank</Typography>
+          <StyledTypography variant="h5" sx={{ flexGrow: 1 }}>
+            A &amp; H Bank
+          </StyledTypography>
+          {/* Add more components or links to the toolbar if needed */}
         </Toolbar>
       </Container>
-    </AppBar>
+    </StyledAppBar>
   );
 };
 
